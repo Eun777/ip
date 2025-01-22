129d93b6a5ebe68b48ef74ce9e936fdd15ec017b
Eunice <e1156977@u.nus.edu>
2025-01-23 00:35:18 +0800

    Add indentations to EunAI replies

--- src/main/java/EunAI.java
+++ src/main/java/EunAI.java
@@ -7,32 +7,33 @@ public class EunAI {
         int maxIndex = -1;
 
 
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm EunAI");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Hello! I'm EunAI");
+        System.out.println("    What can I do for you?");
+        System.out.println("    ____________________________________________________________");
 
         while (true) {
             String input = scanner.nextLine();
 
             if (input.equals("bye")) {
-                System.out.println("____________________________________________________________");
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.println("____________________________________________________________");
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    Bye. Hope to see you again soon!");
+                System.out.println("    ____________________________________________________________");
                 break;
             } else if (input.equals("list")) {
-                System.out.println("____________________________________________________________");
+                System.out.println("    ____________________________________________________________");
                 for (int i = 0; i <= maxIndex; i++) {
+                    System.out.print("      ");
                     System.out.print(i + 1);
                     System.out.println(". " + taskList[i]);
                 }
-                System.out.println("____________________________________________________________");
+                System.out.println("    ____________________________________________________________");
             } else {
                 taskList[maxIndex + 1] = input;
                 maxIndex++;
-                System.out.println("____________________________________________________________");
-                System.out.println("added: " + input);
-                System.out.println("____________________________________________________________");
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    added: " + input);
+                System.out.println("    ____________________________________________________________");
             }
         }
         scanner.close();
