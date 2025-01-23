d8476c17d944f3ea8f83724db4c4afbe9058cd73
Eunice <e1156977@u.nus.edu>
2025-01-24 01:43:18 +0800

    Change from array to ArrayList<Task>

--- src/main/java/EunAI.java
+++ src/main/java/EunAI.java
@@ -1,9 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class EunAI {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        Task[] taskList = new Task[100];
+        ArrayList<Task> taskList = new ArrayList<>();
         int maxIndex = -1;
 
 
@@ -26,7 +27,7 @@ public class EunAI {
                     System.out.print("      ");
                     System.out.print(i + 1);
                     System.out.print(".");
-                    taskList[i].printTask();
+                    (taskList.get(i)).printTask();
                 }
                 System.out.println("    You have " + (maxIndex + 1) + " scheduled tasks and events.");
                 System.out.println("    ____________________________________________________________");
@@ -35,11 +36,11 @@ public class EunAI {
                     int toMark = Integer.parseInt(input.substring(5).trim()) - 1;
 
                     if (toMark >= 0 && toMark <= maxIndex) {
-                        taskList[toMark].markTask();
+                        taskList.get(toMark).markTask();
                         System.out.println("    ____________________________________________________________");
                         System.out.println("    You've completed a task? Good job! I have marked it as done:");
                         System.out.print("      ");
-                        taskList[toMark].printTask();
+                        taskList.get(toMark).printTask();
                         System.out.println("    ____________________________________________________________");
                     } else {
                         System.out.println("    ____________________________________________________________");
@@ -56,11 +57,11 @@ public class EunAI {
                     int toMark = Integer.parseInt(input.substring(7).trim()) - 1;
 
                     if (toMark >= 0 && toMark <= maxIndex) {
-                        taskList[toMark].unmarkTask();
+                        taskList.get(toMark).unmarkTask();
                         System.out.println("    ____________________________________________________________");
                         System.out.println("    You didn't completed the task? Please do it:");
                         System.out.print("      ");
-                        taskList[toMark].printTask();
+                        taskList.get(toMark).printTask();
                         System.out.println("    ____________________________________________________________");
                     } else {
                         System.out.println("    ____________________________________________________________");
@@ -78,11 +79,11 @@ public class EunAI {
                         throw new EmptyTaskException("OOPS!!! The description of a todo cannot be empty.");
                     }
                     String description = input.substring(5).trim(); // Remove "todo"
-                    taskList[maxIndex + 1] = new ToDo(description);
+                    taskList.add(new ToDo(description));
                     maxIndex++;
                     System.out.println("    ____________________________________________________________");
                     System.out.println("    Added the task! Remember to do it!");
-                    System.out.println("    " + taskList[maxIndex].getTaskString());
+                    System.out.println("    " + taskList.get(maxIndex).getTaskString());
                     System.out.println("    You have " + (maxIndex + 1) + " scheduled tasks and events.");
                     System.out.println("    ____________________________________________________________");
                 } catch (EmptyTaskException e) {
@@ -97,11 +98,11 @@ public class EunAI {
                     }
                     String details = input.substring(9).trim();
                     String[] parts = details.split(" /by ");
-                    taskList[maxIndex + 1] = new Deadline(parts[0], parts[1]);
+                    taskList.add(new Deadline(parts[0], parts[1]));
                     maxIndex++;
                     System.out.println("    ____________________________________________________________");
                     System.out.println("    Added the task! Stick to the deadline please :) ");
-                    System.out.println("    " + taskList[maxIndex].getTaskString());
+                    System.out.println("    " + taskList.get(maxIndex).getTaskString());
                     System.out.println("    You have " + (maxIndex + 1) + " scheduled tasks and events.");
                     System.out.println("    ____________________________________________________________");
                 } catch (EmptyTaskException e) {
@@ -119,16 +120,42 @@ public class EunAI {
                     String description = parts[0];
                     String from = parts[1];
                     String to = parts[2];
-                    taskList[maxIndex + 1] = new Event(description, from, to);
+                    taskList.add(new Event(description, from, to));
                     maxIndex++;
                     System.out.println("    ____________________________________________________________");
                     System.out.println("    I have scheduled the event for you!");
-                    System.out.println("    " + taskList[maxIndex].getTaskString());
+                    System.out.println("    " + taskList.get(maxIndex).getTaskString());
                     System.out.println("    You have " + (maxIndex + 1) + " scheduled tasks and events.");
                     System.out.println("    ____________________________________________________________");
                 } catch (EmptyTaskException e) {
                     System.out.println("    ____________________________________________________________");
                     System.out.println("    OOPS!!! The description of an event cannot be empty.");
+                    System.out.println("    ____________________________________________________________");
+                }
+            } else if (input.startsWith("delete")) {
+                try {
+                    if (input.length() <= 7) {
+                        throw new EmptyTaskException("OOPS!!! You forgot which task to delete?");
+                    }
+                    int toDelete = Integer.parseInt(input.substring(7).trim());
+                    if (toDelete > maxIndex + 1) {
+                        throw new TaskOutOfBoundsException("OOPS!!! This task does not even exist. Try again.");
+                    }
+                    System.out.println("    ____________________________________________________________");
+                    System.out.println("    Oki! I've deleted this task since you said so...");
+                    System.out.println("    " + taskList.get(toDelete - 1).getTaskString());
+                    System.out.println("    There's only " + maxIndex + " tasks left in the list.");
+                    System.out.println("    ____________________________________________________________");
+                    taskList.remove(toDelete - 1);
+                    maxIndex--;
+
+                } catch (EmptyTaskException e) {
+                    System.out.println("    ____________________________________________________________");
+                    System.out.println("    OOPS!!! You forgot which task to delete?");
+                    System.out.println("    ____________________________________________________________");
+                } catch (TaskOutOfBoundsException e) {
+                    System.out.println("    ____________________________________________________________");
+                    System.out.println("    OOPS!!! This task does not even exist. Try again.");
                     System.out.println("    ____________________________________________________________");
                 }
             } else {
---
+++ src/main/java/TaskOutOfBoundsException.java
@@ -0,0 +1,5 @@
+public class TaskOutOfBoundsException extends EunAIException {
+    public TaskOutOfBoundsException(String message) {
+        super(message);
+    }
+}
