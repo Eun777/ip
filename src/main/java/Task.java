03647303ab4226ab8071253983bd40dbd060e398
Eunice <e1156977@u.nus.edu>
2025-01-23 10:33:45 +0800

    Add basic functionality for Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,31 @@
+import com.sun.source.tree.Tree;
+
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void printTask() {
+        System.out.println("[" + this.getStatusIcon() + "] " + this.description);
+    }
+
+    public void markTask() {
+        this.isDone = true;
+    }
+
+    public void unmarkTask() {
+        this.isDone = false;
+    }
+
+    public boolean taskStatus() {
+        return this.isDone;
+    }
+}
