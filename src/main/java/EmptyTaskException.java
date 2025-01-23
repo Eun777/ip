841ce579ba763ce27f7490bc8a1f5e629512d225
Eunice <e1156977@u.nus.edu>
2025-01-24 01:14:17 +0800

    Add exceptions for EunAI

---
+++ src/main/java/EmptyTaskException.java
@@ -0,0 +1,5 @@
+public class EmptyTaskException extends eunAIException {
+    public EmptyTaskException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/InvalidCommandException.java
@@ -0,0 +1,5 @@
+public class InvalidCommand extends eunAIException {
+    public InvalidCommand(String message) {
+        super(message);
+    }
+}
