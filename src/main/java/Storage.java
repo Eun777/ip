7fc320338dd45b2596bb884f897b4782a36ba8f9
Eunice <e1156977@u.nus.edu>
2025-01-30 11:04:03 +0800

    Add functionality to Storage class

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,2 +1,63 @@
-package PACKAGE_NAME;public class Storage {
+import java.io.*;
+import java.util.ArrayList;
+public class Storage {
+    private String filePath;
+
+    public Storage(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public void saveTasks(ArrayList<Task> taskList) throws IOException {
+        File file = new File(filePath);
+        file.getParentFile().mkdirs();
+        FileWriter writer = new FileWriter(file);
+
+        for(int i = 0; i < taskList.size(); i++) {
+            writer.write(taskList.get(i).toFileFormat() + System.lineSeparator());
+        }
+
+        writer.close();
+
+    }
+
+    public ArrayList<Task> loadSavedTasks() {
+        ArrayList<Task> prevTaskList = new ArrayList<>();
+        File savedFile = new File(filePath);
+
+        if (!savedFile.exists()) {
+            System.out.println("      You have not saved any information previously. Starting a new list...");
+            return prevTaskList;
+        }
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(savedFile))) {
+            String currLine;
+            while ((currLine = reader.readLine()) != null) {
+                prevTaskList.add(parseTask(currLine));
+            }
+        } catch (IOException e) {
+            System.out.println("        Error loading tasks. Starting a new list...");
+        }
+        return prevTaskList;
+    }
+
+    private Task parseTask(String line) {
+        String[] splitParts = line.split(" \\| ");
+        String taskType = splitParts[0];
+        boolean taskIsDone = splitParts[1].equals("1");
+        String taskDescription = splitParts[2];
+
+        switch (taskType) {
+            case "T":
+                return new ToDo(taskDescription, taskIsDone);
+            case "D":
+                String taskDate = splitParts[3];
+                return new Deadline(taskDescription, taskIsDone, taskDate);
+            case "E":
+                String[] splitDate = splitParts[3].split("-");
+                return new Event(taskDescription, taskIsDone, splitDate[0], splitDate[1]);
+            default:
+                throw new IllegalArgumentException("    Invalid task format.");
+        }
+
+    }
 }
